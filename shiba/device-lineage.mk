d6afb3478f92d2e1455528b8c3142ae37317ba9a
murikbmx <murikbmx@gmail.com>
2024-10-09 00:01:34 +0500

    Bring back: Pixel VoLTE, VoWIFI & 5G Enabler
    Features
    On unsupported countries for the Google Pixel, this module enables and gives you:
    VoLTE
    VoWIFI (Wifi Calling)
    5G (SA+NSA)

--- shiba/device-lineage.mk
+++ shiba/device-lineage.mk
@@ -6,6 +6,7 @@
 
 DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay-lineage
 
+TARGET_VENDOR_PROP += device/google/shusky/shiba/system.prop
 TARGET_VENDOR_PROP += device/google/shusky/shiba/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-lineage.mk)
